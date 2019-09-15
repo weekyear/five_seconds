546f353cf1cfb1c219427551244bdae7da9ddde9
JWeeKyear <jwnsgus@gmail.com>
2019-09-16 00:30:04 +0900

    Icon_Blue 변경

--- Five_Seconds/Five_Seconds/Helpers/CreateDateString.cs
+++ Five_Seconds/Five_Seconds/Helpers/CreateDateString.cs
@@ -74,16 +74,16 @@ namespace Five_Seconds.Helpers
             switch (diffDays.Days)
             {
                 case 0:
-                    dateTimeString = $"오늘 {string.Format("{0:tt} {0:hh}시 {0:mm}분", nextTime)}에";
+                    dateTimeString = $"오늘 {string.Format("{0:tt} {0:hh}시 {0:mm}분", nextTime)}에 실행";
                     break;
                 case 1:
-                    dateTimeString = $"내일 {string.Format("{0:tt} {0:hh}시 {0:mm}분", nextTime)}에";
+                    dateTimeString = $"내일 {string.Format("{0:tt} {0:hh}시 {0:mm}분", nextTime)}에 실행";
                     break;
                 case 2:
-                    dateTimeString = $"모레 {string.Format("{0:tt} {0:hh}시 {0:mm}분", nextTime)}에";
+                    dateTimeString = $"모레 {string.Format("{0:tt} {0:hh}시 {0:mm}분", nextTime)}에 실행";
                     break;
                 default:
-                    dateTimeString = $"{diffDays.Days}일 후 {string.Format("{0:tt} {0:hh}시 {0:mm}분", nextTime)}에";
+                    dateTimeString = $"{diffDays.Days}일 후 {string.Format("{0:tt} {0:hh}시 {0:mm}분", nextTime)}에 실행";
                     break;
 
             }
