1f217c5dfca9f41b08cf39ccba2ccddd16cd8c82
JWeeKyear <jwnsgus@gmail.com>
2019-09-30 00:32:11 +0900

    Record 모델 추가

--- Five_Seconds/Five_Seconds/Models/Record.cs
+++ Five_Seconds/Five_Seconds/Models/Record.cs
@@ -13,10 +13,30 @@ namespace Five_Seconds.Models
         public int Id { get; set; }
 
         [ForeignKey(typeof(Alarm))]
-        public int AlarmId { get; set; }
+        public int AlarmId { get; }
+        
+        public string Name { get; }
 
-        public DateTime Date { get; set; }
-        public int RecordTime { get; set; }
+        public TimeSpan Time
+        {
+            get { return TimeOffset.LocalDateTime.TimeOfDay; }
+        }
+
+        public DateTime Date
+        {
+            get { return TimeOffset.LocalDateTime.Date; }
+        }
+
+        public DateTimeOffset TimeOffset { get; }
+
         public bool IsSuccess { get; set; }
+
+        public Record(Alarm alarm, bool isSuccess)
+        {
+            AlarmId = alarm.Id;
+            Name = alarm.Name;
+            TimeOffset = alarm.TimeOffset;
+            IsSuccess = isSuccess;
+        }
     }
 }
