ae262a85e665a1fb3f0eb2c80e94dd4321d20656
JWeeKyear <jwnsgus@gmail.com>
2019-10-22 16:41:55 +0900

    Add AdMobViewRenderer, AdMobView But Not Use Those.

--- Five_Seconds/Five_Seconds.Android/MainActivity.cs
+++ Five_Seconds/Five_Seconds.Android/MainActivity.cs
@@ -28,6 +28,8 @@ namespace Five_Seconds.Droid
 
             base.OnCreate(savedInstanceState);
 
+            Android.Gms.Ads.MobileAds.Initialize(ApplicationContext, "ca-app-pub-8413101784746060~4411896846");
+
             Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             CrossCurrentActivity.Current.Init(this, savedInstanceState);
